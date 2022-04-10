e6418208a6bdaa8c8471b001cfa9d61be0e41af7
Sebastian Syska <syska.seb@gmail.com>
2022-04-10 18:58:56 +0200

    added material design libs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
-function sayHello() {
-  console.log("dupa log");
-};
+import {Button} from '@material-ui/core'; //importing material ui component
+import {TextField} from '@material-ui/core';
+import SomeHeader from './NavBar.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>dupa</p>
-        <button onClick={sayHello}>Some buttton</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SomeHeader/>
+      <TextField id="name" label="Name" variant="outlined" />
+      <Button color="primary" variant="contained"> Press me </Button>
     </div>
   );
 }
